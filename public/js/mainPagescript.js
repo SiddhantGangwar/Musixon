f7e15c3f3cbc852ac1002a48df33b57dda7c3ffc
Kartik Deepak Dange <kartikdange2002@gmail.com>
2022-12-01 00:58:09 +0530

    Made previous button repeat comptible

--- public/js/mainPagescript.js
+++ public/js/mainPagescript.js
@@ -22,11 +22,11 @@ let isPlaying = false;
 
 // creating a songs obj array 
 let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "/songs/1.mp3", coverPath: "/covers/1.jpg"},
+    {songName: "Warriyo - Mortals ", filePath: "/songs/1.mp3", coverPath: "/covers/1.jpg"},
     {songName: "Cielo - Huma-Huma", filePath: "/songs/2.mp3", coverPath: "/covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "/songs/3.mp3", coverPath: "/covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "/songs/4.mp3", coverPath: "/covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "/songs/5.mp3", coverPath: "/covers/5.jpg"},
+    {songName: "DEAF KEV - Invincible ", filePath: "/songs/3.mp3", coverPath: "/covers/3.jpg"},
+    {songName: "Different Heaven & EH!DE - My Heart ", filePath: "/songs/4.mp3", coverPath: "/covers/4.jpg"},
+    {songName: "Janji-Heroes-Tonight", filePath: "/songs/5.mp3", coverPath: "/covers/5.jpg"},
     {songName: "Rabba - Salam-e-Ishq", filePath: "/songs/2.mp3", coverPath: "/covers/6.jpg"},
     {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "/songs/2.mp3", coverPath: "/covers/7.jpg"},
     {songName: "Bhula Dena - Salam-e-Ishq", filePath: "/songs/2.mp3", coverPath: "/covers/8.jpg"},
@@ -126,17 +126,36 @@ document.getElementById('next').addEventListener('click', ()=>{
 })
 // handele previous button
 document.getElementById('previous').addEventListener('click', ()=>{
-    if(songIndex<=0){
-        songIndex = 0
+    // checking which index to choose according to repeat_state
+    if(repeat_one){
+        songIndex =songIndex;
+    }else{
+        songIndex--;
+        if(songIndex<0 && repeat_state){
+            songIndex = songs.length + songIndex;
+        }
     }
-    else{
-        songIndex -= 1;
-    }
+
     //calling playSong function
-    playSong(songIndex);
+    if(repeat_state || songIndex>=0){
+        playSong(songIndex);
+    }else{
+        audioElement.pause();
+        audioElement.src = ``;
+        masterSongName.innerText = "Song Name";
+        audioElement.currentTime = 0;
+    }
 })
 
 
+//function for random no generation
+function randomIntFromInterval(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+  
+
+
+// playSong function, loads song from given index
 function playSong(songIndex){
     audioElement.pause();
     audioElement.src = `/songs/${songIndex+1}.mp3`;
